2ab30a3662e75c5aca5e24162c1e9fdc5dcef8f1
shoaib <shoibkhan4646@gmail.com>
2020-10-13 20:44:39 +0500

    frontend 1.2

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ $(document).ready(function(){
   $(".dots").click(function(){
 	 $("#rem").toggleClass("mobhide");
   });
+
+
+
+
+    
+     jQuery('.home-section-hide').slideToggle('.home-section-hide');
+        if(jQuery('#iconchange11').hasClass('fa-angle-down')){
+jQuery('#iconchange11').removeClass('fa-angle-down');
+jQuery('#iconchange11').addClass('fa-angle-up');
+          jQuery(".barmob22").html("Read Less");
+}
+else{
+jQuery('#iconchange11').removeClass('fa-angle-up');
+jQuery('#iconchange11').addClass('fa-angle-down');
+  jQuery(".barmob22").html("Read More");
+}
+});
+
+
+
+
+  
 });
 
 
@@ -14,3 +36,5 @@ $(document).ready(function(){
         $(this).attr('placeholder',$(this).data('holder'));
     });
 })
+
+
